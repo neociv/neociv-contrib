3a3a0fd14786bce32488add52505fce5b5ed28b1
murt <murt@unpossible.io>
2022-12-20 13:37:01 +1000

    Added basic outline of manifest and package contents

---
+++ package/index.lua
@@ -0,0 +1,6 @@
+-- Define currencies
+cvl.define("currency", { id = "gold", title = "Gold", icon = { vec = "@/icons/currencies/gold.svg" } })
+
+-- Define units
+-- TODO: Add functionality such as movement filters, attacks, meshes, materials, etc... y'know, all the hard stuff
+cvl.define("unit", { id = "common.warrior", title = "Warrior" })
