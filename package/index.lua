9039c93ae4172bd904c6860a6fdacae56a921ed8
murt <murt@unpossible.io>
2022-12-22 16:07:56 +1000

    WIP of setting up cell filters for units

--- package/index.lua
+++ package/index.lua
@@ -1,6 +1,8 @@
+require "./filters/movement.cvl"
+
 -- Define currencies
-cvl.define("currency", { id = "gold", title = "Gold", icon = { vec = "@/icons/currencies/gold.svg" } })
+cvl.define("currency", "gold", { title = "Gold", icon = { vec = "@/icons/currencies/gold.svg" } })
 
 -- Define units
 -- TODO: Add functionality such as movement filters, attacks, meshes, materials, etc... y'know, all the hard stuff
-cvl.define("unit", { id = "common.warrior", title = "Warrior" })
+cvl.define("unit", "common.warrior", { title = "Warrior", default_movement_filter = "$.filters.ground.simple" })
